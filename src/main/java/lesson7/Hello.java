7392cc15d212dea44dd6d4c5cc0dd1d6a62c9055
Baurzhan Dosym <baurzhan.zh@gmail.com>
2021-02-02 22:58:10 -0500

    Lesson 7 - intro to git ++

--- src/main/java/lesson7/Hello.java
+++ src/main/java/lesson7/Hello.java
@@ -3,5 +3,6 @@ package lesson7;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Hello, students");
     }
 }
