19e3e9df7326f91aca3c3c8114071239189cfa00
AndreyT <robinhood.ex@gmail.com>
2021-02-04 22:53:32 -0500

    Lesson8

---
+++ src/main/java/lesson8/Lesson8.java
@@ -0,0 +1,33 @@
+package lesson8;
+
+public class Lesson8 {
+    public static void main(String[] args) {
+        Person alex = new Person();
+        alex.name = "Alex";
+        alex.lastName = "Geiman";
+        alex.yearOfBirth = 1999;
+
+        System.out.println(alex.lastName);
+        Person jane = new Person();
+        jane.name = "Jane";
+        jane.lastName = "Popper";
+        jane.yearOfBirth = 2001;
+
+        alex.hello(jane.name);
+        Person maria = new Person();
+        maria.name = "Maria";
+        maria.lastName = "Small";
+        maria.yearOfBirth = jane.yearOfBirth;
+
+        Vehicle myCar = new Vehicle();
+        myCar.color = "Green";
+        myCar.make = "Toyota";
+        myCar.model = "Supra";
+        myCar.year = 2015;
+
+        myCar.drive();
+        myCar.honk();
+
+
+    }
+}
---
+++ src/main/java/lesson8/Person.java
@@ -0,0 +1,13 @@
+package lesson8;
+
+public class Person {
+    public String name;
+    public String lastName;
+    public int yearOfBirth;
+
+    public void hello(String aName) {
+        System.out.println(name+" is saying hello to "+aName);
+    }
+}
+
+
