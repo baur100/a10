d68aaf2015c5d44db9d938e46a2bc506a5a345a4
Baurzhan Dosym <baurzhan.zh@gmail.com>
2021-02-04 22:51:19 -0500

    Lesson 8 - Classes, Objects, Fields, Methods

---
+++ src/main/java/lesson8/Vehicle.java
@@ -0,0 +1,15 @@
+package lesson8;
+
+public class Vehicle {
+    public String make;
+    public String model;
+    public int year;
+    public String color;
+
+    public void drive(){
+        System.out.println(make+ " "+ model +" is driving");
+    }
+    public void beep(){
+        System.out.println(color+" car is beeping");
+    }
+}
