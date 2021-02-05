87f9046220af177966cd898dca44d2638a1369e1
AndreyT <robinhood.ex@gmail.com>
2021-02-04 22:53:32 -0500

    Lesson8

---
+++ src/main/java/lesson8/Vehicle.java
@@ -0,0 +1,18 @@
+package lesson8;
+
+public class Vehicle {
+    public String make;
+    public String model;
+    public int year;
+    public String color;
+
+    public void drive() {
+        System.out.println(make+ " "+model +" is driving");
+    }
+    public void honk() {
+        System.out.println(make+ " "+model +" is honking");
+    }
+
+}
+
+
