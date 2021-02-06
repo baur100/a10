23d350e9d7b23e5a6a36a99476cf535cb62c57a7
Baurzhan Dosym <baurzhan.zh@gmail.com>
2021-02-06 17:06:16 -0500

    JavaCore - Lesson 9 - Getters Setters Constructors, Encapsulation

--- src/main/java/lesson9/Person1.java
+++ src/main/java/lesson9/Person1.java
@@ -1,46 +1,42 @@
 package lesson9;
 
-import java.time.temporal.ValueRange;
 
 public class Person1 {
     private String name;
     private String lastName;
     private int year;
 
-    public Person1(String name, String lastName, int year) {
-        this.name = name;
-        this.lastName = lastName;
-        this.year = year;
-    }
-
-    public Person1(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
-
-    public Person1() {}
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
+    public Person1(String name, String lastName,int year){
+        setYear(year);
+        this.lastName=lastName;
         this.name=name;
     }
-    public void setYear (int year1) {
-        if(year1>2021 || year1<1900) {
-            throw new IllegalArgumentException("Wrong year");
-        }
-        year=year1;
+    public Person1(String name, String lastName){
+        this.name=name;
+        this.lastName=lastName;
     }
+    public Person1(){}
 
-    public int getYear() {
-        return year;
-    }
-    public String getLastName() {
+    public String getLastName(){
         return this.lastName;
     }
-
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName){
         this.lastName=lastName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setYear(int year){
+        if(year>2021 || year<1900){
+            throw new IllegalArgumentException("Wrong year\n - year must be in the interval\n between 1900 and 2021 - you provided "+year);
+        }
+        this.year=year;
+    }
+    public int getYear(){
+        return this.year;
     }
 }
