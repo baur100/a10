f64ffe894b74b3469727e96c95c3997875afc4db
Zarinchos <sharakhmetovva@gmail.com>
2021-02-08 22:19:09 -0700

    HM9

---
+++ src/main/java/HM9/APP.java
@@ -0,0 +1,12 @@
+package HM9;
+
+public class APP {
+    public static void main(String[] args) {
+        AdoptionCenter   a1 = new AdoptionCenter("DogDora","white",123,100,"PetStore");
+         Zoo z1= new Zoo("MonkeyDenny","Black",12548,"BoiseZoo");
+
+
+         z1.name();
+         a1.GetPet();
+    }
+}
