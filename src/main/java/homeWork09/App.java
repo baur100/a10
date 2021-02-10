a48d48711bc649f84bdd6b15621377ed509aa8c5
AndreyT <robinhood.ex@gmail.com>
2021-02-09 21:15:57 -0500

    HoweWork for 9-10 lessons FIXED

--- src/main/java/homeWork09/App.java
+++ src/main/java/homeWork09/App.java
@@ -20,11 +20,11 @@ public class App {
         carl.printClass();
         System.out.println(carl.getFence());
         Mansion joe = new Mansion();
-        joe.name = "Joe's mansion";
-        joe.squareFeet = 2140;
-        joe.floors = 2;
-        joe.fence = Fenses.RED;
-        joe.tenants = joeTenants;
+        joe.setName("Joe's mansion");
+        joe.setSquareFeet(2140);
+        joe.setFloors(2);
+        joe.setFence(Fenses.RED);
+        joe.setTenants(joeTenants);
         joe.printClass();
 
 
@@ -33,15 +33,27 @@ public class App {
         Castle king = new Castle("Old castle", 3820, 5, johnTenants, Fenses.BLACK, extras);
         king.printClass();
         Castle henryVII = new Castle();
-        henryVII.name = "Castle of HenryVII";
-        henryVII.squareFeet = 8020;
-        henryVII.floors = 7;
-        henryVII.tenants = henryTenants;
-        henryVII.fence = Fenses.BLUE;
-        henryVII.extra = extras2;
+        henryVII.setName("Castle of HenryVII");
+        henryVII.setSquareFeet(8020);
+        henryVII.setFloors(7);
+        henryVII.setTenants(henryTenants);
+        henryVII.setFence(Fenses.BLUE);
+        henryVII.setExtras(extras2);
         henryVII.printClass();
 
+        System.out.println("----------------------------------------------");
 
+        int[] for1 = {-2, 0, 5, 9, 3, 2};
+        Condition[] cond1 = {Condition.RAIN, Condition.WINDY};
+        Condition[] cond2 = {Condition.SUNNY};
+        Forecast newYork = new Forecast("New York", for1, cond1);
+        Forecast newJersey = new Forecast();
+
+        newYork.printClass();
+
+        newJersey.setLocation("New Jersey");
+        newJersey.setConditions(cond2);
+        System.out.println(newJersey.getLocation());
 
 
 
