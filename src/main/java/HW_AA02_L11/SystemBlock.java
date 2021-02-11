a688ffa7f2fbceb0b73ec12c7269e803af3db616
aas020280 <aas020280@gmail.com>
2021-02-11 12:43:25 -0800

    HW 11 from Alexey

--- src/main/java/HW_AA02_L11/SystemBlock.java
+++ src/main/java/HW_AA02_L11/SystemBlock.java
@@ -5,4 +5,43 @@ public class SystemBlock {
     private VideoAdapter videoAdapter;
     private PowerSuply powerSuply;
     private Disk disk;
+
+    public SystemBlock(MotherBoard motherBoard, VideoAdapter videoAdapter, PowerSuply powerSuply, Disk disk) {
+        this.motherBoard = motherBoard;
+        this.videoAdapter = videoAdapter;
+        this.powerSuply = powerSuply;
+        this.disk = disk;
+    }
+
+    public MotherBoard getMotherBoard() {
+        return motherBoard;
+    }
+
+    public void setMotherBoard(MotherBoard motherBoard) {
+        this.motherBoard = motherBoard;
+    }
+
+    public VideoAdapter getVideoAdapter() {
+        return videoAdapter;
+    }
+
+    public void setVideoAdapter(VideoAdapter videoAdapter) {
+        this.videoAdapter = videoAdapter;
+    }
+
+    public PowerSuply getPowerSuply() {
+        return powerSuply;
+    }
+
+    public void setPowerSuply(PowerSuply powerSuply) {
+        this.powerSuply = powerSuply;
+    }
+
+    public Disk getDisk() {
+        return disk;
+    }
+
+    public void setDisk(Disk disk) {
+        this.disk = disk;
+    }
 }
