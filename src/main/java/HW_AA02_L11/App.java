4750ad0a3f9ac9aadfe7e0b482aadb975b925ddb
aas020280 <aas020280@gmail.com>
2021-02-11 10:41:18 -0800

    Renamed hw11 to HW_AA02_L11

--- src/main/java/HW_AA02_L11/App.java
+++ src/main/java/HW_AA02_L11/App.java
@@ -1,4 +1,4 @@
-package hw11;
+package HW_AA02_L11;
 
 public class App {
     public static void main(String[] args) {
--- src/main/java/HW_AA02_L11/Keyboard.java
+++ src/main/java/HW_AA02_L11/Keyboard.java
@@ -1,4 +1,4 @@
-package hw11;
+package HW_AA02_L11;
 
 public class Keyboard {
     private String brand;
