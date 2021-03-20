2209ce2ba9a235b5480a9082917be8b8e96aa870
AndreyT <robinhood.ex@gmail.com>
2021-03-20 16:08:06 -0400

    MyTestNG

--- src/test/java/unitTesting/UnitTestParametrized.java
+++ src/test/java/unitTesting/UnitTestParametrized.java
@@ -1,24 +1,24 @@
-package unitTesting;
-
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-import tickets.CalculateTicketPrice;
-
-public class UnitTestParametrized {
-    @DataProvider (name = "testData")
-    public Object[][] getTestData() {
-        return new Object[][] {
-                {1, 1000, 0.0},
-                {5, 1000, 500.0},
-                {29, 1000, 1000.0},
-                {75, 1000, 800.0},
-        };
-    }
-    @Test(dataProvider = "testData")
-    public void calculateTicketPrice(int age, int fare, double price) {
-        CalculateTicketPrice person = new CalculateTicketPrice(fare, age);
-        double result = person.getPrice();
-        Assert.assertEquals(price, result);
-    }
-}
+//package unitTesting;
+//
+//import org.testng.Assert;
+//import org.testng.annotations.DataProvider;
+//import org.testng.annotations.Test;
+//import tickets.CalculateTicketPrice;
+//
+//public class UnitTestParametrized {
+//    @DataProvider (name = "testData")
+//    public Object[][] getTestData() {
+//        return new Object[][] {
+//                {1, 1000, 0.0},
+//                {5, 1000, 500.0},
+//                {29, 1000, 1000.0},
+//                {75, 1000, 800.0},
+//        };
+//    }
+////    @Test(dataProvider = "testData")
+////    public void calculateTicketPrice(int age, int fare, double price) {
+////        CalculateTicketPrice person = new CalculateTicketPrice(fare, age);
+////        double result = person.getPrice();
+////        Assert.assertEquals(price, result);
+////    }
+//}
