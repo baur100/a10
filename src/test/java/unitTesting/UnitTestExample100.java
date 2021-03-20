fe214cb62bc3601959f916cd71525200af649abf
Baurzhan Dosym <baurzhan.zh@gmail.com>
2021-03-20 16:08:50 -0400

    TEST-1234 first commit

--- src/test/java/unitTesting/UnitTestExample100.java
+++ src/test/java/unitTesting/UnitTestExample100.java
@@ -4,29 +4,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import tickets.CalculateTicketPrice;
 
-public class UnitTestExample {
+public class UnitTestExample100 {
     @Test
     public void calculateTicketPrice_infant_returnsZero(){
-        CalculateTicketPrice infant = new CalculateTicketPrice(500,1);
+        CalculateTicketPrice infant = new CalculateTicketPrice(100,1);
         double price = infant.getPrice();
         Assert.assertEquals(price,0.0);
     }
     @Test
     public void calculateTicketPrice_child_50percent(){
-        CalculateTicketPrice child = new CalculateTicketPrice(500,10);
+        CalculateTicketPrice child = new CalculateTicketPrice(100,10);
         double price = child.getPrice();
-        Assert.assertEquals(price,250.0);
+        Assert.assertEquals(price,50.0);
     }
     @Test
     public void calculateTicketPrice_adult_fullPrice(){
-        CalculateTicketPrice adult = new CalculateTicketPrice(500,35);
+        CalculateTicketPrice adult = new CalculateTicketPrice(100,35);
         double price=adult.getPrice();
-        Assert.assertEquals(price,500.0);
+        Assert.assertEquals(price,100.0);
     }
     @Test
     public void calculateTicketPrice_senior_80percent(){
-        CalculateTicketPrice senior = new CalculateTicketPrice(500,70);
+        CalculateTicketPrice senior = new CalculateTicketPrice(100,70);
         double price = senior.getPrice();
-        Assert.assertEquals(price,400.0);
+        Assert.assertEquals(price,80.0);
     }
 }
